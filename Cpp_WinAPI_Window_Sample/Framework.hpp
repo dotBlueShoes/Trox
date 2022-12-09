cfce50e1bd65afcf365365474e91f8076f6f4a28
Matthew <40423487+dotBlueShoes@users.noreply.github.com>
2022-12-09 12:38:51 +0100

    Initialize mst-structure

---
+++ Cpp_WinAPI_Window_Sample/Framework.hpp
@@ -0,0 +1,20 @@
+﻿// header.h: plik dołączany dla standardowych systemowych plików dołączanych,
+// lub pliki dołączane specyficzne dla projektu
+//
+
+#pragma once
+
+#include "mst.hpp"						// MST standard library
+
+#include "settings/targetver.h"			// WinAPI version control - GUI look.
+
+#define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
+#include <windows.h>					// Pliki nagłówkowe systemu Windows
+
+
+// Pliki nagłówkowe środowiska uruchomieniowego języka C
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
